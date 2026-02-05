f24936f2952dda8f7dc40a895253c947a7c715be
Xeon Rapture <contact@xeonrapture.com>
2026-02-05 14:52:28 -0700

    change nev

--- shared/nav.js
+++ shared/nav.js
@@ -3,7 +3,7 @@
   if (!host) return;
 
   // Load nav HTML
-  const res = await fetch("https://app.xeonrapture.com/nav.html");
+  const res = await fetch("../shared/nav.html");
   const html = await res.text();
   host.innerHTML = html;
 
@@ -46,7 +46,7 @@
     if (logoutBtn) {
       logoutBtn.onclick = async () => {
         await sb.auth.signOut();
-        window.location.href = "https://app.xeonrapture.com/login.html";
+        window.location.href = "../shared/login.html";
       };
     }
   }
