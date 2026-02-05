70d027eaa8c3c6606858d6c83c2cf9a961b572d0
Xeon Rapture <contact@xeonrapture.com>
2026-02-05 11:32:20 -0700

    Update auth.js

--- auth.js
+++ auth.js
@@ -1,4 +1,4 @@
-import { createClient } from "@supabase/supabase-js";
+import { createClient } from "https://cdn.jsdelivr.net/npm/@supabase/supabase-js@2";
 
 export const supabase = createClient(
   "https://tustchcydbvipwcxeqfq.supabase.co",
